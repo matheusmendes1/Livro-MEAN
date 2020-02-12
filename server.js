c4167a6ac14e49c8a6c5a918b51d27faf31fea91
Matheus Santos <matheusmendes1@hotmail.com>
2020-02-11 21:53:44 -0300

    End Chapter 3.5

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ http
     .listen(app.get('port'), () => {
         console.log(`Express Server running on port ${app.get('port')}`);
     });
+
+// Lembrar de rodar o 'npm install nodemon -g' e o 'npm install bower -g' além do npm install, num novo projeto.
